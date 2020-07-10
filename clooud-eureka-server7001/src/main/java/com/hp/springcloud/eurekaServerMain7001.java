beb36d072028086ca952ba5c794f89daed0851d0
liu <1712624418@qq.com>
2020-07-10 23:48:56 +0800

    eureka

--- clooud-eureka-server7001/src/main/java/com/hp/springcloud/eurekaServerMain7001.java
+++ clooud-eureka-server7001/src/main/java/com/hp/springcloud/eurekaServerMain7001.java
@@ -2,6 +2,7 @@ package com.hp.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
