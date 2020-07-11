e81a712781dc4ac6bea14826408b41b51d138d0e
liu <1712624418@qq.com>
2020-07-11 16:15:59 +0800

    eureka

---
+++ cloud-provider-payment8002/src/main/java/com/hp/springcloud/PaymentMain8002.java
@@ -0,0 +1,21 @@
+package com.hp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName PaymentMain8001
+ * @Description TODO
+ * @Author 17126
+ * @Date 2020/7/7 0:02
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class,args);
+    }
+}
